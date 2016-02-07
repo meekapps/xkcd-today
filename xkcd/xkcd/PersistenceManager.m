1d9ec544e4f3254a8b5c3ad394ee299e2cec6773
Keller <keller@perka.com>
2016-02-07 15:16:30 -0500

    removed core data abort()

--- xkcd/xkcd/PersistenceManager.m
+++ xkcd/xkcd/PersistenceManager.m
@@ -55,10 +55,8 @@
     dict[NSLocalizedFailureReasonErrorKey] = failureReason;
     dict[NSUnderlyingErrorKey] = error;
     error = [NSError errorWithDomain:[NSBundle mainBundle].bundleIdentifier code:9999 userInfo:dict];
-    // Replace this with code to handle the error appropriately.
-    // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+
     NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
-    abort();
   }
   
   return _persistentStoreCoordinator;
