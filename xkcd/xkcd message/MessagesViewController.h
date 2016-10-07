058f7987901a7bc3201ccfeeec9de17695727bf5
Keller <keller@meekapps.net>
2016-10-07 19:17:58 -0400

    created iMessage target

---
+++ xkcd/xkcd message/MessagesViewController.h
@@ -0,0 +1,13 @@
+//
+//  MessagesViewController.h
+//  xkcd message
+//
+//  Created by Mike Keller on 10/7/16.
+//  Copyright © 2016 Perka. All rights reserved.
+//
+
+#import <Messages/Messages.h>
+
+@interface MessagesViewController : MSMessagesAppViewController
+
+@end
