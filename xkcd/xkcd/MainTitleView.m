47c3340e4f891f37270f257b0eb28da2b6278238
keller <mikeller@grubhub.com>
2018-02-25 21:47:08 -0500

    added timer

--- xkcd/xkcd/MainTitleView.m
+++ xkcd/xkcd/MainTitleView.m
@@ -10,15 +10,26 @@
 #import "NSDate+ShortDate.h"
 #import "XKCDComic.h"
 
-typedef NS_ENUM(NSUInteger, MainTitleViewState) {
-  MainTitleViewTitleState = 0,
-  MainTitleViewDateState = 1,
-  MainTitleViewIndexState = 2,
+typedef NS_ENUM(NSUInteger, MainTitleAnimationDirection) {
+  MainTitleAnimationDirectionNone,
+  MaintitleAnimationDirectionDown,
+  MaintitleAnimationDirectionUp,
 };
+
+typedef NS_ENUM(NSUInteger, MainTitleViewState) {
+  MainTitleViewTitleState,
+  MainTitleViewDateState,
+  MainTitleViewIndexState,
+};
+
+static CFTimeInterval const kAnimationDuration = 0.3;
+static CGFloat const kMainTitleViewDefaultFontSize = 17.0F;
+static NSTimeInterval const kTitleTimerDuration = 3.0;
 
 @interface MainTitleView()
 @property (strong, nonatomic) UILabel *titleLabel;
 @property (nonatomic) MainTitleViewState state;
+@property (strong, nonatomic) NSTimer *timer;
 @end
 
 @implementation MainTitleView
@@ -29,6 +40,13 @@ typedef NS_ENUM(NSUInteger, MainTitleViewState) {
   if (self) {
     _comic = comic;
     _state = MainTitleViewTitleState;
+    self.clipsToBounds = YES;
+    
+    _titleLabel = [[UILabel alloc] initWithFrame:self.bounds];
+    _titleLabel.numberOfLines = 2;
+    _titleLabel.textAlignment = NSTextAlignmentCenter;
+    _titleLabel.font = [UIFont boldSystemFontOfSize:kMainTitleViewDefaultFontSize];
+    [self addSubview:_titleLabel];
     
     UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleTap:)];
     [self addGestureRecognizer:tap];
@@ -36,35 +54,55 @@ typedef NS_ENUM(NSUInteger, MainTitleViewState) {
   return self;
 }
 
-- (UILabel *)titleLabel {
-  if (!_titleLabel) {
-    _titleLabel = [[UILabel alloc] initWithFrame:self.bounds];
-    _titleLabel.numberOfLines = 2;
-    _titleLabel.textAlignment = NSTextAlignmentCenter;
-    _titleLabel.font = [UIFont boldSystemFontOfSize:18.0F];
-    [self addSubview:_titleLabel];
-  }
-  return _titleLabel;
-}
-
 - (void)setComic:(XKCDComic *)comic {
   _comic = comic;
+  self.state = MainTitleViewTitleState;
   [self updateTitle];
+  [self invalidateTimerIfNeeded];
 }
 
 - (void)setState:(MainTitleViewState)state {
+  _state = state;
   [self updateTitle];
 }
 
 #pragma mark - Private
 
+- (void) addTimerIfNeeded {
+  [self invalidateTimerIfNeeded];
+  
+  if (self.state == MainTitleViewTitleState) return;
+  
+  self.timer = [NSTimer scheduledTimerWithTimeInterval:kTitleTimerDuration
+                                                target:self
+                                              selector:@selector(resetStateFromTimer:)
+                                              userInfo:nil
+                                               repeats:NO];
+}
+
 - (void) handleTap:(UITapGestureRecognizer *)tapRecognizer {
   self.state = self.state == MainTitleViewIndexState ? MainTitleViewTitleState : self.state + 1;
-  [self updateTitle];
-  // TODO fix this
+  [self updateTitleAnimationDirection:MaintitleAnimationDirectionDown];
+  [self addTimerIfNeeded];
+}
+
+- (void) invalidateTimerIfNeeded {
+  if (self.timer.isValid) {
+    [self.timer invalidate];
+  }
+}
+
+- (void) resetStateFromTimer:(NSTimer *)timer {
+  self.state = MainTitleViewTitleState;
+  
+  [self updateTitleAnimationDirection:MaintitleAnimationDirectionUp];
 }
 
 - (void) updateTitle {
+  [self updateTitleAnimationDirection:MainTitleAnimationDirectionNone];
+}
+
+- (void) updateTitleAnimationDirection:(MainTitleAnimationDirection)animationDirection {
   switch (self.state) {
     case MainTitleViewTitleState:
       self.titleLabel.text = self.comic.title;
@@ -73,11 +111,20 @@ typedef NS_ENUM(NSUInteger, MainTitleViewState) {
       self.titleLabel.text = [self.comic.date shortDate];
       break;
     case MainTitleViewIndexState:
-      self.titleLabel.text = [NSString stringWithFormat:@"%@", self.comic.index];
+      self.titleLabel.text = [NSString stringWithFormat:@"# %@", self.comic.index];
       break;
     default:
       // Do nothing
       break;
+  }
+  
+  if (animationDirection != MainTitleAnimationDirectionNone) {
+    CATransition *transition = [CATransition animation];
+    transition.duration = kAnimationDuration;
+    transition.type = kCATransitionPush;
+    transition.subtype = animationDirection == MaintitleAnimationDirectionDown ? kCATransitionFromBottom : kCATransitionFromTop;
+    transition.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+    [self.titleLabel.layer addAnimation:transition forKey:@"slide"];
   }
 }
 
